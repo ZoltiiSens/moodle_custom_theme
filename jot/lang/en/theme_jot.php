2cd648c0065a584010f16a1b2591f0d06d9be654
Roman <zoltiisen@gmail.com>
2023-12-06 20:46:02 +0200

    Initial commit. Basic moodle theme sceleton(min required files)

---
+++ jot/lang/en/theme_jot.php
@@ -0,0 +1,12 @@
+<?php
+// Every file should have GPL and copyright in the header - I skip it for now.
+
+// This line protects the file from being accessed by a URL directly.                                                               
+defined('MOODLE_INTERNAL') || die();
+
+// A description shown in the admin theme selector.                                                                                 
+$string['choosereadme'] = 'Jot test theme (is child of Boost).';                
+// The name of our plugin.                                                                                                          
+$string['pluginname'] = 'JOT test theme';                                                                                                    
+// We need to include a lang string for each block region.                                                                          
+$string['region-side-pre'] = 'Right';
